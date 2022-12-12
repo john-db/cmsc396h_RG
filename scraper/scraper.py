63762df5c53dcc71de689e000e6aa8cc1ff68045
john-db <119277936+john-db@users.noreply.github.com>
2022-12-12 11:21:34 -0500

    Update and rename scraper.py to scraper/scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -7,7 +7,7 @@ sites["Root Domain"] = sites["Root Domain"].astype("string")
 websites = sites["Root Domain"].apply(lambda x: "http://" + x)
 
 driver = webdriver.Firefox()
-driver.install_addon(path='./v1', temporary=True)
+driver.install_addon(path='../v1', temporary=True)
 
 for link in websites[0:40]:
     try:
